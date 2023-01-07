1e4c4a4d65563d2848be69d7d5f5bb3e18496ba2
Joachim <joachimkjera@gmail.com>
2023-01-07 10:06:18 +0100

    wip exercise

---
+++ NutritionCalc.Web/NutritionCalc.Web/Models/Exercise.cs
@@ -0,0 +1,27 @@
+﻿namespace NutritionCalc.Web.Models
+{
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public List<Set> Sets { get; set; }
+        public string Note { get; set; }
+    }
+
+    public class Activity
+    {
+        public string Name { get; set; }
+        public string Note { get; set; }
+    }
+
+    public class Set
+    {
+        public int Reps { get; set; }
+        public double Weight { get; set; }
+        public TimeOnly Rest { get; set; }
+    }
+    
+    public class Repetition 
+    {
+        public int Count { get; set; }
+    }    
+}
